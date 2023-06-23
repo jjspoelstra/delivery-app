32631b1c6f00856f87219538935d849b4e10c9ce
Joe Spoelstra <123589895+jjspoelstra@users.noreply.github.com>
2023-06-23 16:07:29 -0400

    Update dynamodb.js
    To create user and get delivery by id

--- dynamodb.js
+++ dynamodb.js
@@ -22,6 +22,38 @@ const getUserById = async (id) => {
    }
 }
 
+const getDeliveryById = async (id) -> {
+   const params = {
+      TableName: 'Deliveries',
+      Item: {
+         id: Number(id),
+      },
+   };
+   try {
+      const data = await dynamodb.get(params).promise()
+      console.log('Delivery:' data.Item)
+   } catch (error) {
+      console.error('Error getting user', error)
+   }
+}
+
+const createUser = async (user) => {
+   const paramas = {
+      TableName: 'User',
+      Item: {
+         ...user,
+         id: Number(user.id), 
+      },
+   };
+
+   try {
+      await dynamodb.put(params).promise()
+      console.log('User created successfully.')
+   } catch (error) {
+      console.error("Error creating delivery:', error)
+      }
+   }
+
 const createDelivery = async (delivery) => {
     const params = {
       TableName: 'Deliveries',
@@ -41,6 +73,7 @@ const createDelivery = async (delivery) => {
   
   // Call the functions as needed
   getUserById('01');
+   getDeliveryById('01')
 //   createDelivery({
 //     id: '456',
 //     description: 'Example delivery',
